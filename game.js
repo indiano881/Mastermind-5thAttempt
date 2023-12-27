5a9c89303b5b0c0d1816931805d471958d9f1687
Davide Baldi <d.baldi88@gmail.com>
2023-12-27 14:53:23 +0100

    js START, data color system created-secret code is working-NEED TO STYLE CONTAINER to visaulize

---
+++ game.js
@@ -0,0 +1,24 @@
+$(()=>{
+
+
+//arrays    
+const COLOUR_DATABASE = ["red","orange","yellow","green","blue","purple","pink","brown","aqua"];
+let copy_COLOUR_DATABASE = Array.from(COLOUR_DATABASE);
+let computerColourChoice = new Array (4);
+let removeColourIndex;
+
+
+//Computer colour array generator
+for (let i = 0; i < computerColourChoice.length; i++) {
+    removeColourIndex = Math.floor(Math.random() * copy_COLOUR_DATABASE.length);
+    computerColourChoice[i] = copy_COLOUR_DATABASE.splice(removeColourIndex, 1)[0];
+}
+
+for (let i=1; i<5; i++){
+    $(".secret-color"+i).attr("data-color",computerColourChoice[i-1]);
+
+}
+
+
+
+})
